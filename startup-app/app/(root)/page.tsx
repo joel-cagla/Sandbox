77e46c47ba59ed41b74cb531f011f1031bccc4b2
joel-cagla <joel@cagla.jp>
2025-03-14 15:17:48 +0900

    Add query parameters for search bar

--- startup-app/app/(root)/page.tsx
+++ startup-app/app/(root)/page.tsx
@@ -2,7 +2,10 @@
 
 import SearchForm from "../components/SearchForm";
 
-export default function Home() {
+export default async function Home({ searchParams}: {
+  searchParams: Promise<{query?: string}>
+}) {
+  const query = (await searchParams).query;
   return (
     <>
       <section className="pink_container">
@@ -13,7 +16,7 @@ export default function Home() {
           className="sub-heading !max-w-3xl">Submit your ideas, vote on pitches and get noticed in 
             competitions!
           </p>
-          <SearchForm />
+          <SearchForm query={query} />
       </section>
     </>
   );
--- startup-app/app/components/SearchForm.tsx
+++ startup-app/app/components/SearchForm.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import Form from "next/form";
 import SearchFormReset from "./SearchFormReset";
 
-const SearchForm = () => {
-    const query = "test query";
-
+const SearchForm = ({query}: {query?:string}) => {
     return (
         <Form action="/" scroll={false} className="search-form">
             <input 
@@ -16,6 +14,7 @@ const SearchForm = () => {
 
             <div className="flex gap-2">
                 {query && <SearchFormReset />}
+                <button type="submit" className="search-btn text-white">Search</button>
             </div>
         </Form>
     )
