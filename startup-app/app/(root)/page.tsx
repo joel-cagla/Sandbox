4117dbd129617b048e5eb0eddd43339628b87d37
joel-cagla <joel@cagla.jp>
2025-03-14 11:10:10 +0900

    Add tailwind class testing snippet
    Added a snippet to test whether tailwind is rendering classes properly.

--- startup-app/app/(root)/page.tsx
+++ startup-app/app/(root)/page.tsx
@@ -3,7 +3,8 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-2xl">HOME</h1>
+    {/* <div className="bg-blue-500 text-white text-xl font-bold p-4">HELLO TAILWIND</div> */}
+    <h1 className="heading">HOME</h1>
     </>
   );
 }
