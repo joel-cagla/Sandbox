b4d795876963379d02a1881b7c5c5db83bd40616
joel-cagla <joel@cagla.jp>
2025-03-18 14:21:39 +0900

    Minor code fixes

--- startup-app/auth.ts
+++ startup-app/auth.ts
@@ -1,13 +1,13 @@
-import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
-import { author_by_github_id_query } from "./sanity/lib/queries"
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import { author_by_github_id_query } from "./sanity/lib/queries";
 import { client } from "@/sanity/lib/client";
 import { writeClient } from "./sanity/lib/write-client";
  
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [GitHub],
   callbacks: {
-    async signIn({ user: { name, email, image }, account, profile: { id, login, bio } }) {
+    async signIn({ user: { name, email, image }, profile: { id, login, bio } }) {
       const existingUser = await client.withConfig({ useCdn: false }).fetch(author_by_github_id_query, { id });
 
       if(!existingUser){
@@ -26,7 +26,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
 
   async jwt({ token, account, profile }) {
     if (account && profile) {
-      const user = await client.fetch(author_by_github_id_query, { id: profile?.id });
+      const user = await client.withConfig({ useCdn: false }).fetch(author_by_github_id_query, { id: profile?.id });
 
       token.id = user?._id;
     }
